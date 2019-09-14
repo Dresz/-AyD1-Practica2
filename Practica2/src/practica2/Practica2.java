5ce29ae1ab5a86f0f52e75d527e712940e2b929f
LuisFML <luisf18.360@gmail.com>
2019-09-14 16:36:46 -0600

    Prueba conexcion

--- Practica2/src/practica2/Practica2.java
+++ Practica2/src/practica2/Practica2.java
@@ -2,17 +2,15 @@ package practica2;
 
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import service.Connection;
 
 public class Practica2 {
 
     public static void main(String[] args) throws ClassNotFoundException, 
             InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
         
-        //UIManager.setLookAndFeel("com.jtattoo.plaf.mcwin.McWinLookAndFeel");
-        //Login login=new Login();
-        //login.show();
-        Connection.obtener();
+        UIManager.setLookAndFeel("com.jtattoo.plaf.mcwin.McWinLookAndFeel");
+        Login login=new Login();
+        login.show();
     }
     
 }
